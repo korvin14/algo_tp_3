795b42b1f0015a8a1f6aa4782984d4fbdaefc1ba
Dmitry Ermachenkov <korvin14@gmail.com>
2016-03-22 04:04:01 +0300

    started 3rd problem

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * algo_tp_3/main.cpp
+ *
+ *   Created on: Mar 22, 2016
+ *       Author: Dmitry Ermachenkov
+ *      License: free-to-use if you refernce me
+ *  Description: ​Дан массив целых чисел А[0..n­1]. Известно, что на интервале [0, m] значения массива
+ *  строго возрастают, а на интервале [m, n­1] строго убывают. Найти m за O(log m). n ≤ 10000.
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(void) {
+  cout << "Hello bitches" << endl;
+  return 0;
+}
